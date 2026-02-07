772cb7f9fbf07841f189f6cd348a8230cee9cc1d
Valeria Cardozo <vcardozoc@unitecnica.net>
2026-02-06 20:49:05 -0500

    Trabajo en clase

--- ejemplo.js
+++ ejemplo.js
@@ -1,1 +1,14 @@
-console.log("Hola, este es un ejemplo de codigo JavaScript");   
+console.log("Hola, este es un ejemplo de codigo JavaScript");
+
+let edad = 22; 
+let nombre, apellido;
+
+nombre= "Valeria";
+apellido= "Cardozo";
+
+const telefono = "322-662-0430"; 
+
+nombre= "Salome"; // 
+
+console.log(nombre, apellido, "tiene", edad, "años y su telefono es", telefono)
+
