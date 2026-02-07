ba7e7887faa9113e1e8ed3ea5e4527ad5cdf7436
Valeria Cardozo <vcardozoc@unitecnica.net>
2026-02-06 23:23:24 -0500

    Taller 1 JavaScript Valeria Cardozo Carmona

---
+++ variable.js
@@ -0,0 +1,9 @@
+let estudiante = "Valeria Cardozo";
+let grupo = 1191;
+let curso = "JavaScript Básico";
+let clase = "Definición de variables y constantes";
+
+console.log("Estudiante:", estudiante);
+console.log("Grupo:", grupo);
+console.log("Curso:", curso);
+console.log("Clase:", clase);
